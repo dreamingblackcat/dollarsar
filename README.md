ba38b573525cb2e427d1f32ed12c690b85550a49
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-10-13 15:20:49 +0630

    Initial app working

---
+++ README.md
@@ -0,0 +1,6 @@
+# MM Exchange Rate Extension
+
+
+#Credits
+
+- Extension icon is downloaded from [here](https://icons8.com/web-app/7391/currency-exchange)
---
+++ js/popup.js
@@ -0,0 +1,48 @@
+Vue.component('converter', {
+  template: '#converter-template',
+  props: ['currency_name', 'rate', 'bank_name'],
+  data: function(){
+    return {
+      type: "buy",
+      foreignAmount: 1 ,
+      mmkAmount: ""
+    };
+  },
+  computed: {
+    mmk: function(){
+      return this.foreignAmount * this.rate[this.type]; 
+    } 
+  },
+  beforeUpdate: function(){
+    if (this.type == "buy") {
+      this.foreignAmount = accounting.unformat(this.foreignAmount);
+      this.mmkAmount = accounting.formatMoney( this.foreignAmount
+                         * +(this.rate[this.type]),
+                         "MMK "
+                       );
+    } else {
+      this.mmkAmount = accounting.unformat(this.mmkAmount);
+      this.foreignAmount = accounting.formatMoney( this.mmkAmount / +(this.rate[this.type]));
+    }
+  }
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    message: {}
+  },
+  created: function(){
+    console.log("Created");
+    this.getLatestRate(); 
+  },
+  methods: {
+    getLatestRate: function () {
+
+      this.$http.get("http://localhost:8000/api/v1/latest")
+                .then(function(res){
+                  this.message = res.body.data; 
+                }.bind(this));
+    }
+  } }); 
+
